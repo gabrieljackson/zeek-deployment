1731cb858ad576c21a0a81490bcc99bbcc577a70
Gabe Jackson <3694686+gabrieljackson@users.noreply.github.com>
2025-08-31 13:35:42 -0400

    Print grafana login details on deployment completion

--- scripts/build-and-deploy.sh
+++ scripts/build-and-deploy.sh
@@ -4,28 +4,44 @@
 
 set -euo pipefail
 
-echo ""
-echo "### BUILDING CUSTOM ZEEK IMAGE"
+wait_for_k8s_resource() {
+  local resource=$1
+  local name=$2
+  local namespace=$3
+
+  for i in {1..15}; do
+    if $(kubectl get $resource -n $namespace $name > /dev/null 2>&1); then
+      return
+    fi
+    sleep 1
+  done
+  
+  exit 1
+}
+
+echo -e "\n### BUILDING CUSTOM ZEEK IMAGE"
 docker build -t zeek-with-extras:0.0.1 zeek/
 
-echo ""
-echo "### CREATING K3D CLUSTER"
+echo -e "\n### CREATING K3D CLUSTER"
 CLUSTER_NAME="zeek"
 k3d cluster create $CLUSTER_NAME -p "80:80@loadbalancer" -p "443:443@loadbalancer"
 k3d image import zeek-with-extras:0.0.1 -c $CLUSTER_NAME
 
-echo ""
-echo "### DEPLOYING GRAFANA"
+echo -e "\n### DEPLOYING GRAFANA"
 kubectl apply -f grafana/grafana.yaml
 
-echo ""
-echo "### DEPLOYING LOKI"
+echo -e "\n### DEPLOYING LOKI"
 kubectl apply -f loki/loki.yaml
 
-echo ""
-echo "### DEPLOYING ZEEK"
+echo -e "\n### DEPLOYING ZEEK"
 kubectl apply -f zeek/fluent-bit-config.yaml
 kubectl apply -f zeek/zeek.yaml
 
-echo ""
-echo "### DONE!"
+echo -e "\nWaiting for Grafana secret to exist..."
+wait_for_k8s_resource secret grafana grafana
+
+echo -e "\n### DEPLOYMENT COMPLETE!"
+
+echo -e "\n### GRAFANA LOGIN DETAILS:"
+echo "Username: admin"
+echo "Password: $(kubectl get secret --namespace grafana grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo)"
